6796a0bbaa22413d647be7dc2f1ce0bdfbd92576
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2025-06-07 18:59:47 +0530

    Update numpy-06.py

--- numpy-06.py
+++ numpy-06.py
@@ -30,3 +30,28 @@ b_np = np.arange(10000000, 20000000)
 start = time.time()
 c_np = a_np + b_np  # Vectorized operation
 print("Time taken with NumPy arrays:", time.time() - start)
+
+print("-----------------------")
+
+import sys
+
+
+# Python lists addition
+a_list = [i for i in range(10000000)]
+print(sys.getsizeof(a_list))
+
+# NumPy array addition
+a_np = np.arange(10000000)
+print(sys.getsizeof(a_np))
+
+a_np = np.arange(10000000, dtype=np.int)
+print(sys.getsizeof(a_np))
+
+print("-----------------------")
+
+
+"""
+NumPy is much more memory-efficient and faster due to fixed-type storage and contiguous memory.
+Use NumPy if you're dealing with large numeric datasets.
+"""
+
